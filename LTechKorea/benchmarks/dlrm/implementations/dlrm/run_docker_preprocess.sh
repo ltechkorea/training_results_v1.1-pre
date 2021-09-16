8be9914b5e7f0fa5bcfd59ccb7fb7508782c546c
dong <dong095362@naver.com>
2021-09-16 10:32:23 +0900

    add_preprocess

--- LTechKorea/benchmarks/dlrm/implementations/dlrm/run_docker_preprocess.sh
+++ LTechKorea/benchmarks/dlrm/implementations/dlrm/run_docker_preprocess.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DATA_DIR=/opt/data/Dataset/training/dlrm			# path to data folder
+export DATA_DIR=/opt/data/Dataset/training/dlrm			# path to data folder
 
 docker run -it --rm --network=host --ipc=host --shm-size=1g --ulimit memlock=-1 \
            --ulimit stack=67108864 --gpus=all \
-           -v $DATA_DIR:/code/data \
-           -e DATA_DIR=/code/data \
+           -v $DATA_DIR:/data \
            dlrm_preprocess \
+
 
